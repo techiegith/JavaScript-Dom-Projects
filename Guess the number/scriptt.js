8b67665a128be18f21e75735f35f3433c831d318
techiegith <128968069+techiegith@users.noreply.github.com>
2023-12-28 18:18:51 +0530

    Add files via upload

---
+++ Guess the number/scriptt.js
@@ -0,0 +1,97 @@
+// const form =document.querySelector('.form');
+// console.log(parseInt(Math.random() * 100 +1));
+let randomNumber = parseInt(Math.random() * 100 +1);
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
+
+const p = document.createElement('p');
+
+let prevGuess = [];
+let numGuess = 1;
+
+let playGame = true;
+
+if(playGame){
+    submit.addEventListener('click',function(e){
+        e.preventDefault();
+        const guess = parseInt(userInput.value);
+        console.log(guess);
+        validateGuess(guess);
+    });
+}
+
+function validateGuess(guess){
+    if(isNaN(guess)){
+        alert('please enter the valid number')
+    }else if(guess < 1){
+        alert('please enter a number more than 1')
+    }
+    else if(guess > 100){
+        alert('please enter a number less than 100')
+    }
+    else{
+        prevGuess.push(guess)
+        if(numGuess === 11){
+            displayGuess(guess)
+            displayMessgae(`Game Over . Random number was ${randomNumber}`)
+            endGame()
+        }
+        else{
+            displayGuess(guess)
+            checkGuess(guess)
+        }
+    }
+}
+
+function checkGuess(guess){
+    if(guess === randomNumber){
+        displayMessgae(`You guessed it right`);
+        endGame()
+    }
+    else if(guess < randomNumber){
+        displayMessgae(`Number is too low`);
+    }
+    else if(guess > randomNumber){
+        displayMessgae(`Number is too high`)
+    }
+}
+// cleanup guess
+function displayGuess(guess){
+    userInput.value = ''   // UPDATE KR RHE HAI EMPTY  STRING
+    guessSlot.innerHTML += `${guess}     `;
+    numGuess++;
+    remaining.innerHTML = `${11-numGuess}`;
+}
+
+function displayMessgae(Messgae){
+    lowOrHi.innerHTML = `<h2>${Messgae}</h2>`;
+}
+
+function endGame(){
+    userInput.value = ''
+    userInput.setAttribute('disabled','')
+    p.classList.add('button')
+    p.innerHTML = `<h2 id = "newGame"> Start New Game</h2>`;
+    startOver.appendChild(p)
+    playGame = false
+    newGame()
+}
+function newGame(){
+     const newGameButton = document.querySelector('#newGame')
+     newGameButton.addEventListener('click',function(e){
+        randomNumber = parseInt(Math.random() * 100 +1);
+        prevGuess = []
+        numGuess = 1
+        guessSlot.innerHTML = ''
+        remaining.innerHTML = `${11 - numGuess}`;
+        userInput.removeAttribute('disabled')
+        startOver.removeChild(p)
+
+        playGame = true
+
+     });
+}
